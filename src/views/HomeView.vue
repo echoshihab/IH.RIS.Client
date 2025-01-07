54c8c0b2d14d261068916eded6f55505b4e2b5a8
Shihab Khan <echoshihab@gmail.com>
2025-01-06 23:11:02 -0500

    add vuetify and first pass at patient search form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="search-form">
+      <v-text-field label="Patient Search"></v-text-field>
+      <v-select
+        label="Select"
+        :items="['MRN', 'HCN', 'Demographic Details']"
+        variant="outlined"
+      ></v-select>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.search-form {
+  display: flex;
+  flex-direction: row;
+  flex: 2 1 0;
+}
+</style>
