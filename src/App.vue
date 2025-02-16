ecc6eefedffc0baecb848942c0b5cc58078a1d5f
Shihab Khan <echoshihab@gmail.com>
2025-02-16 16:19:33 -0500

    listen for snackbar events in main app template

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
+  <v-snackbar v-model="show" :timeout="timeout">
+    {{ message }}
+  </v-snackbar>
+
   <header>
     <div class="wrapper"></div>
   </header>
 
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useSnackbarStore } from '@/stores/snackbarStore'
+
+const snackbarStore = useSnackbarStore()
+const { show, message, timeout } = storeToRefs(snackbarStore)
+</script>
 
 <style scoped></style>
