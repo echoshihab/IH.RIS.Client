3725068d0a67041a31165234d4b0aad2704d899b
Shihab Khan <echoshihab@gmail.com>
2025-04-23 15:43:45 -0400

    add basic and advanced patient search tab

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,31 @@
 <template>
   <main>
-    <div class="search-form">
-      <v-text-field label="Patient Search" v-model="searchValue"></v-text-field>
-      <v-select
-        label="Select"
-        :items="['MRN', 'HCN', 'Demographic Details']"
-        v-model="searchType"
-        variant="outlined"
-      ></v-select>
+    <v-btn-toggle v-model="toggle" divided class="mb-4">
+      <v-btn value="mrn">Basic </v-btn>
+      <v-btn value="advanced">Advanced </v-btn>
+    </v-btn-toggle>
+    <v-form v-if="showAdvanced">
+      <v-text-field label="First Name"></v-text-field>
+      <v-text-field label="Middle Name"></v-text-field>
+      <v-text-field label="Last Name"></v-text-field>
+      <v-text-field label="Gender"> </v-text-field>
+      <v-date-input label="Birthdate"></v-date-input>
+    </v-form>
 
-      <v-btn @click="getPatient">Search</v-btn>
-    </div>
+    <v-text-field label="MRN" v-model="searchValue" v-else></v-text-field>
+
+    <v-btn @click="getPatient">Search</v-btn>
   </main>
 </template>
 
 <script setup lang="ts">
 import type { Patient } from '@/Models/Patient'
 import { PatientService } from '@/services/patientService'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+
+const toggle = ref('mrn')
+
+const showAdvanced = computed(() => toggle.value == 'advanced')
 
 let patientService = new PatientService()
 
@@ -28,7 +36,7 @@ const searchValue = ref<string>()
 const searchType = ref<string>()
 
 async function getPatient() {
-  if (searchType.value === 'MRN') {
+  if (toggle.value === 'mrn') {
     patient.value = await patientService.get(searchValue.value)
     console.log(patient.value)
   }
