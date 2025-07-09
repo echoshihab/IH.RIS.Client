85d8eeccfed713ba5e6d091f67b904fd6f89462c
Shihab Khan <echoshihab@gmail.com>
2025-07-08 21:34:58 -0400

    bind patient properties to patient edit inputs

--- src/components/Patient.vue
+++ src/components/Patient.vue
@@ -7,28 +7,39 @@
   >
     <v-progress-circular indeterminate color="primary" size="64" />
   </v-overlay>
-  <v-card
-    class="mx-auto"
-    prepend-icon="$vuetify"
-    subtitle="The #1 Vue UI Library"
-    width="400"
-  >
-    <template v-slot:title v-if="patient">
+  <v-card class="mx-auto" prepend-icon="$vuetify" width="400" v-if="patient">
+    <template v-slot:title>
       <span class="font-weight-black">Patient MRN : {{ patient.mrn }}</span>
     </template>
 
-    <v-card-text class="bg-surface-light pt-4">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione
-      debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat
-      totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+    <v-card-text class="bg-surface-light pt-4 mb-2">
+      <v-text-field
+        label="First Name"
+        v-model="patient.name.firstName"
+      ></v-text-field>
+      <v-text-field
+        label="Middle Name"
+        v-model="patient.name.middleName"
+      ></v-text-field>
+      <v-text-field
+        label="Last Name"
+        v-model="patient.name.lastName"
+      ></v-text-field>
+      <v-date-input
+        label="Date of Birth"
+        prepend-icon=""
+        prepend-inner-icon="$calendar"
+        v-model="patientDob"
+      ></v-date-input>
     </v-card-text>
+    <v-btn class="d-flex justify-center" color="indigo-darken-3"> Save </v-btn>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import type { Patient } from '@/Models/Patient'
 import { usePatientStore } from '@/stores/patientStore'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const loading = ref(false)
@@ -38,6 +49,23 @@ const route = useRoute()
 const patientStore = usePatientStore()
 
 const patient = ref<Patient>()
+
+const patientDob = computed({
+  get() {
+    if (patient.value && patient.value.birthYear) {
+      const { birthYear, birthMonth, birthDay } = patient.value
+      return new Date(birthYear, birthMonth - 1, birthDay)
+    }
+    return null
+  },
+  set(newBirthDate) {
+    if (newBirthDate && patient.value) {
+      patient.value.birthYear = newBirthDate.getFullYear()
+      patient.value.birthMonth = newBirthDate.getMonth() + 1
+      patient.value.birthDay = newBirthDate.getDate()
+    }
+  },
+})
 
 onMounted(() => {
   loading.value = true
