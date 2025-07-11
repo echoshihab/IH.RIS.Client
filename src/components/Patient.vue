acb410e4c5416c4c2762b532018c6e4bea5754b2
Shihab Khan <echoshihab@gmail.com>
2025-07-10 20:47:46 -0400

    add read only view for patient details with option to switch to edit view

--- src/components/Patient.vue
+++ src/components/Patient.vue
@@ -7,12 +7,29 @@
   >
     <v-progress-circular indeterminate color="primary" size="64" />
   </v-overlay>
+
   <v-card class="mx-auto" prepend-icon="$vuetify" width="400" v-if="patient">
     <template v-slot:title>
-      <span class="font-weight-black">Patient MRN : {{ patient.mrn }}</span>
-    </template>
+      <div class="d-flex justify-space-between">
+        <span class="font-weight-black">Patient MRN : {{ patient.mrn }}</span>
 
-    <v-card-text class="bg-surface-light pt-4 mb-2">
+        <v-icon
+          color="teal-darken-2"
+          icon="mdi-eye"
+          size="small"
+          v-if="editMode"
+          @click="toggleEditMode"
+        ></v-icon>
+        <v-icon
+          v-else
+          color="teal-darken-2"
+          icon="mdi-account-edit"
+          size="small"
+          @click="toggleEditMode"
+        ></v-icon>
+      </div>
+    </template>
+    <v-card-text class="bg-surface-light pt-4 mb-2" v-if="editMode">
       <v-text-field
         label="First Name"
         v-model="patient.name.firstName"
@@ -32,14 +49,36 @@
         v-model="patientDob"
       ></v-date-input>
     </v-card-text>
-    <v-btn class="d-flex justify-center" color="indigo-darken-3"> Save </v-btn>
+
+    <v-card-text class="bg-surface-light pt-4 mb-2" v-else>
+      <v-list density="compact">
+        <v-list-subheader>Patient Details</v-list-subheader>
+
+        <v-list-item color="primary">
+          <template v-slot:prepend>
+            <v-label class="mr-2">Name: </v-label>
+          </template>
+          <v-list-item-title
+            class="text-capitalize"
+            v-text="getPatientFullName()"
+          ></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+    <v-btn
+      v-if="editMode"
+      class="d-flex justify-center"
+      color="indigo-darken-3"
+    >
+      Save
+    </v-btn>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import type { Patient } from '@/Models/Patient'
 import { usePatientStore } from '@/stores/patientStore'
-import { computed, onMounted, ref } from 'vue'
+import { capitalize, computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const loading = ref(false)
@@ -48,7 +87,21 @@ const route = useRoute()
 
 const patientStore = usePatientStore()
 
+const getPatientFullName = () =>
+  [
+    patient.value?.name.firstName,
+    patient.value?.name.middleName,
+    patient.value?.name.lastName,
+  ]
+    .filter(v => !!v)
+    .join(' ')
+
 const patient = ref<Patient>()
+
+const editMode = ref(false)
+const toggleEditMode = function () {
+  editMode.value = !editMode.value
+}
 
 const patientDob = computed({
   get() {
